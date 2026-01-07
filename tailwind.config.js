43471b1a53c0b4b6abca56e73edc88fc01801e35
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-01-07 13:40:20 +0530

    Add responsive design enhancements and new SVG icons for improved UI

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    screens: {
+      xs: '480px',   // 👈 custom breakpoint
+      sm: '640px',   // default
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+  },
+}
