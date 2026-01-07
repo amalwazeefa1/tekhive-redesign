bf5e269b77b000903194f8e877f0cf08b1823515
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-01-07 15:06:07 +0530

    counter

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".fade-section").forEach((section) => {
-    gsap.from(section, {
-        opacity: 0,
-        y: 60,
-        duration: 1,
-        ease: "power3.out",
-        scrollTrigger: {
-            trigger: section,
-            start: "top 80%",
-            toggleActions: "play none none none"
-        }
-    });
-});
+// gsap.utils.toArray(".fade-section").forEach((section) => {
+//     gsap.from(section, {
+//         opacity: 0,
+//         y: 60,
+//         duration: 1,
+//         ease: "power3.out",
+//         scrollTrigger: {
+//             trigger: section,
+//             start: "top 80%",
+//             toggleActions: "play none none none"
+//         }
+//     });
+// });
+
+
+        const counters = document.querySelectorAll(".counter");
+
+        counters.forEach(counter => {
+            const target = +counter.dataset.target;
+            let current = 0;
+            const increment = Math.ceil(target / 150);
+
+            const update = () => {
+                if (current < target) {
+                    current += increment;
+                    counter.textContent = current;
+                    requestAnimationFrame(update);
+                } else {
+                    counter.textContent = target;
+                }
+            };
+
+            update();
+        });
