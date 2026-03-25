5ab8742adf0e23ff5a7ede83fb26e75451de5a1d
amalwazeefa1 <amalwazeefa1@gmail.com>
2026-03-25 18:15:08 +0530

    updated home

--- script.js
+++ script.js
@@ -937,7 +937,7 @@ if (aboutHero && ball) {
 }
 
 ///////////////////////////////////////////////////////mask animation - text reveal
-const maskTargets = [".text-mask-banner", ".text-mask-section-about", ".text-mask1-section-home", ".text-mask2-section-home", ".text-mask-section-services", ".text-mask-testimonials", ".text-mask-approach", ".text-mask-contact", ".text-mask-about-label"];
+const maskTargets = [".text-mask-banner", ".text-mask-section-about", ".text-mask1-section-home", ".text-mask2-section-home", ".text-mask-section-services", ".text-mask-testimonials", ".text-mask-approach", ".text-mask-contact", ".text-mask-about-label", ".text-mask-agile"];
 
 maskTargets.forEach(selector => {
     const elements = gsap.utils.toArray(selector);
